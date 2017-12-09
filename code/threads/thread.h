e57816995a0f69dd1a04a1cc9228290e9cd0d261
chunyou0830 <chunyou0830@gmail.com>
2017-12-09 23:14:48 +0800

    Add prototype

--- code/threads/thread.h
+++ code/threads/thread.h
@@ -107,6 +107,16 @@ class Thread {
     void Print() { cout << name; }
     void SelfTest();		// test whether thread impl is working
 
+    // Added -----------------------
+    bool Thread::setPriority(int pri);
+    int Thread::getPriority();
+    void Thread::setReadyTime();
+    int Thread::getReadyTime();
+    void Thread::setBurstTime(double t);
+    int Thread::getBurstTime();
+    void Thread::startBurst();
+    int Thread::getstartBurst();
+
   private:
     // some of the private data for this class is listed above
     
